98183d1801f670d3a18d39bea2e2f1b60a7dadfb
Ashwin Srikant <asrikant@cori05.nersc.gov>
2019-04-19 16:18:01 -0700

    Reads points from file, and calculates similarities in 2D

--- clustering/example/example.cpp
+++ clustering/example/example.cpp
@@ -5,20 +5,41 @@
 #include <Eigen/Core>
 #include "SpectralClustering.h"
 #include <iostream>
+#include <fstream>
+
+
+int calculate_similarity(std::pair<double, double> a, std::pair<double, double> b) {
+    return exp(-1 * sqrt(pow(a.first - b.first, 2) + pow(a.second - b.second, 2)) / 100);
+}
 
 int main() {
-    std::vector<int> items = {1,2,3,4,5,6,7,8,9,10};
+    std::vector<std::pair<double, double> > points;
+    std::ifstream file("points.txt");
+    std::string line;
+
+    while (std::getline(file, line)) {
+        std::stringstream linestream(line);
+        std::string x;
+        std::string y;
+
+        linestream >> x >> y;
+        points.push_back(std::make_pair(std::stod(x), std::stod(y)));
+
+    }
+
+    std::cout << points.size() << std::endl; 
+    //std::vector<int> items = {1,2,3,4,5,16,17,18,19,20};
 
     // generate similarity matrix
-    unsigned int size = items.size();
+    unsigned int size = points.size();
     unsigned int numClusters = 2;
     Eigen::MatrixXd m = Eigen::MatrixXd::Zero(size,size);
 
     for (unsigned int i=0; i < size; i++) {
         for (unsigned int j=0; j < size; j++) {
             // generate similarity
-            int d = items[i] - items[j];
-            int similarity = exp(-d*d / 100);
+            double similarity = calculate_similarity(points[i], points[j]);
+           // std::cout << similarity << std::endl;
             m(i,j) = similarity;
             m(j,i) = similarity;
         }
@@ -43,9 +64,9 @@ int main() {
 
     // output clustered items
     // items are ordered according to distance from cluster centre
-    for (unsigned int i=0; i < clusters.size(); i++) {
+    /*for (unsigned int i=0; i < clusters.size(); i++) {
         std::cout << "Cluster " << i << ": " << "Item ";
         std::copy(clusters[i].begin(), clusters[i].end(), std::ostream_iterator<int>(std::cout, ", "));
         std::cout << std::endl;
-    }
+    } */
 }
